0e6da82d2cafde1fa71f71af32de82cf7fa2fffa
Joel Cross <joel@kazbak.co.uk>
2018-06-14 17:59:11 +0100

    Improve setup.py formatting

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(
-    name='django_multivalueformfield',
-    description='Multiple-value field for Django forms',
-    version='0.0.1',
-    url='https://github.com/ukch/django_multivalueformfield',
-    author='Joel Cross',
-    author_email='joel@kazbak.co.uk',
-    py_modules=['multivaluefield'],
-    license='MIT',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Framework :: Django :: 2.0',
-    ],
-    install_requires=[
-        'Django>=2.0',
-    ],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_multivalueformfield",
+    version="0.0.1",
+    author="Joel Cross",
+    author_email="joel@kazbak.co.uk",
+    description="Multiple-value field for Django forms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ukch/django_multivalueformfield",
+    py_modules=["multivaluefield"],
+    license="MIT",
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django :: 2.0",
+    ),
+    install_requires=(
+        "Django>=2.0",
+    ),
 )
